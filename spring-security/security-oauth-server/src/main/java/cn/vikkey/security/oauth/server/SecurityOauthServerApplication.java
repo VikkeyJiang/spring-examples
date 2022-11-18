6d9a63dc076d15b6a90e80db3be6bdd1c485ce31
VikkeyJiang <j_yy2008@163.com>
2022-11-18 23:03:26 +0800

    初始化oauth-server工程

---
+++ spring-security/security-oauth-server/src/main/java/cn/vikkey/security/oauth/server/SecurityOauthServerApplication.java
@@ -0,0 +1,13 @@
+package cn.vikkey.security.oauth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOauthServerApplication.class, args);
+    }
+
+}
---
+++ spring-security/security-oauth-server/src/test/java/cn/vikkey/security/oauth/server/SecurityOauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.vikkey.security.oauth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOauthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
